6890d47ffed6b1cb78d6d16fd54a5f12f5fbb586
Khoa Nguyen <khoa.tan.nguyen.96@gmail.com>
2023-10-31 16:23:19 -0700

    test: explicitly set timezone

--- vitest.config.js
+++ vitest.config.js
@@ -1,5 +1,5 @@
-import {fileURLToPath} from 'node:url';
-import {mergeConfig, defineConfig, configDefaults} from 'vitest/config'
+import { fileURLToPath } from 'node:url'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
@@ -8,6 +8,7 @@ export default mergeConfig(
     test: {
       globals: true,
       environment: 'jsdom',
+      globalSetup: './test/test-globals.js',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
